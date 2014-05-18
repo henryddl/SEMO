2b7609bbf59d580601bf11f1aed299535a541823
Marlon <marlon.menjivar_@hotmail.com>
2014-05-18 00:37:57 -0600

    Iconos cambiados

--- SEMOv1/gen/ues/semo/R.java
+++ SEMOv1/gen/ues/semo/R.java
@@ -22,12 +22,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int gear=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int lista_ic=0x7f020002;
-        public static final int paint=0x7f020003;
-        public static final int pass_ic=0x7f020004;
-        public static final int search_ic=0x7f020005;
-        public static final int user_ic=0x7f020006;
+        public static final int ic_action_paste=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int lista_ic=0x7f020004;
+        public static final int paint=0x7f020005;
+        public static final int pass_ic=0x7f020006;
+        public static final int user_ic=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f08000c;
