4d36c8477d7cb0826de399bee5d828c3500ac865
Marlon <marlon.menjivar_@hotmail.com>
2014-05-17 20:14:01 -0600

    Pantallas de Login, y alumno main

--- SEMOv1/gen/ues/semo/R.java
+++ SEMOv1/gen/ues/semo/R.java
@@ -21,21 +21,57 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int gear=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int lista_ic=0x7f020002;
+        public static final int paint=0x7f020003;
+        public static final int pass_ic=0x7f020004;
+        public static final int search_ic=0x7f020005;
+        public static final int user_ic=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int administrarCuentaItem=0x7f08000a;
+        public static final int buscarItem=0x7f080008;
+        public static final int button1=0x7f080003;
+        public static final int cerrarSesionItem=0x7f08000c;
+        public static final int editText1=0x7f080001;
+        public static final int editText2=0x7f080002;
+        public static final int listView1=0x7f080005;
+        public static final int listaCursosItem=0x7f080009;
+        public static final int progressBar1=0x7f080004;
+        public static final int salir=0x7f080006;
+        public static final int scrollView1=0x7f080000;
+        public static final int verNotasItem=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
+        public static final int activity_main_administrador=0x7f030001;
+        public static final int activity_main_alumno=0x7f030002;
+        public static final int activity_main_docente=0x7f030003;
     }
     public static final class menu {
         public static final int login=0x7f070000;
+        public static final int main_administrador=0x7f070001;
+        public static final int main_alumno=0x7f070002;
+        public static final int main_docente=0x7f070003;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int administrarCuentaLabel=0x7f050009;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int buscarCursoLabel=0x7f05000c;
+        public static final int cerrarSesionLabel=0x7f05000b;
+        public static final int contrasenia_string=0x7f050004;
+        public static final int hello_world=0x7f05000d;
+        public static final int iniciarSesionString=0x7f050005;
+        public static final int login_string=0x7f050002;
+        public static final int mostrarListaLabel=0x7f05000e;
+        public static final int salir_string=0x7f050001;
+        public static final int title_activity_main_administrador=0x7f050008;
+        public static final int title_activity_main_alumno=0x7f050006;
+        public static final int title_activity_main_docente=0x7f050007;
+        public static final int usuario_string=0x7f050003;
+        public static final int verNotasLabel=0x7f05000a;
     }
     public static final class style {
         /** 
--- SEMOv1/src/sv/ues/fia/semo/activity/Login.java
+++ SEMOv1/src/sv/ues/fia/semo/activity/Login.java
@@ -3,14 +3,23 @@ package sv.ues.fia.semo.activity;
 import ues.semo.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class Login extends Activity {
+	public final static String EXTRA_MESSAGE1 = "sv.ues.fia.semo.activity.userMESSAGE";
+	public final static String EXTRA_MESSAGE2 = "sv.ues.fia.semo.activity.passMESSAGE";
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		setContentView(R.layout.activity_login);	    
 	}
 
 	@Override
@@ -19,5 +28,99 @@ public class Login extends Activity {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.salir:
+	            finish();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	//Método asociado al onClick del botón "Iniciar Sesión"
+	public void Ingresar(View view){
+		EditText userEDTXT =(EditText)findViewById(R.id.editText1);
+		EditText passEDTXT =(EditText)findViewById(R.id.editText2);
+		ProgressBar barraCarga=(ProgressBar)findViewById(R.id.progressBar1);
+		
+		//Contiene el valor de usuario
+		String user=userEDTXT.getText().toString();
+		//Contiene el valor de contraseña
+		String pass=passEDTXT.getText().toString();
+		
+
+		//If que sirve para comprobar que los datos esten completos
+		if(user.equals("")||pass.equals("")){
+			Toast.makeText(this, "Datos incompletos.", Toast.LENGTH_SHORT).show();
+		}
+		else{
+			//Si hay datos ingresados
+			//Se activa Barra de Progreso
+			barraCarga.setVisibility(View.VISIBLE);
+
+			/**En este punto se debería comprobar que el usuario exista y que la contraseña sea correcta y
+			   se debería retornar el código asociado al tipo de cuenta* 
+			   Para el ejemplo se usará un if para manejar la situación de datos incorrectos o correctos*/
+			
+			//Variables ficticia SOLO PARA EJEMPLO
+			boolean auxiliar=true;
+			int tipoUsuario=0;
+			
+			//If que servira para manejar los dos casos de existencia o no existencia:
+			if(auxiliar){
+				
+				/**En este punto se decidirá si enviar al main del alumno,docente o administrador basandose en
+				   el tipo de cuenta retornado*/ 
+				
+				//ALUMNO=0,DOCENTE=1,ADMINISTRADOR=2;
+				Intent intent;
+				switch(tipoUsuario){
+					case 0:
+						//Ejecutando Activity Principal de menu de alumno
+						intent = new Intent(this, MainAlumno.class);
+						intent.putExtra(EXTRA_MESSAGE1, user);
+						intent.putExtra(EXTRA_MESSAGE2, pass);
+						//Desactiva barra de progreso
+						barraCarga.setVisibility(View.GONE);
+					    startActivity(intent);
+						Toast.makeText(this, "Tipo Alumno.", Toast.LENGTH_SHORT).show();
+						break;
+					case 1:
+						intent = new Intent(this, MainDocente.class);
+						intent.putExtra(EXTRA_MESSAGE1, user);
+						intent.putExtra(EXTRA_MESSAGE2, pass);
+						//Desactiva barra de progreso
+						barraCarga.setVisibility(View.GONE);
+					    startActivity(intent);
+						Toast.makeText(this, "Tipo Docente.", Toast.LENGTH_SHORT).show();
+						break;
+					case 2:
+						intent = new Intent(this, MainAdministrador.class);
+						intent.putExtra(EXTRA_MESSAGE1, user);
+						intent.putExtra(EXTRA_MESSAGE2, pass);
+						//Desactiva barra de progreso
+						barraCarga.setVisibility(View.GONE);
+					    startActivity(intent);
+						Toast.makeText(this, "Tipo Administrador.", Toast.LENGTH_SHORT).show();
+						break;
+					
+				}//fin del switch
+
+			}//fin del if
+			else{
+				//Se indicará en el toast que los datos son incorrectos
+				Toast.makeText(this, "Error de autentificación.", Toast.LENGTH_SHORT).show();
+				//Descativa Barra de Progreso
+				barraCarga.setVisibility(View.GONE);
+			}
+			
+		}
+	}//Fin de onClick
+		
+		
 
 }
